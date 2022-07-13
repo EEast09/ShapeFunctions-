1bbc74f49373483db8e8cafc994bf3420bf47878
Erinn East <erinn.east@gmail.com>
2022-07-13 17:17:17 -0400

    Fist commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function getAreaOfCircle(radius){
+return getAreaOfCircle(4)
+}
+
+let pi = 3.14
+
+function  getAreaOfCircle(radius){
+    console.log(pi*radius**2)
+    return pi*radius**2
+}
+getAreaOfCircle(4)
+
+
+function  getCircumferenceOfCircle(radius){
+    console.log(2*pi*radius)
+    return 2*pi*radius
+}
+getCircumferenceOfCircle(7)
+
+
+function  getAreaOfSquare(side){
+    console.log(side * side)
+    return  side * side
+}
+getAreaOfSquare(6)
+
+
+function getAreaOfTriangle(base, height){
+    console.log(base*height);
+    return base * height;
+ 
+}
+getAreaOfTriangle(4,4)
